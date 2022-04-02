09f50777245ce700b410d0b3c431164d08dee50a
Lemorax99 <erudito_01@hotmail.com>
2022-04-01 18:49:11 -0600

    Añadir instrucciones de uso del api

--- DB/Query_creacionDB.sql
+++ DB/Query_creacionDB.sql
@@ -3,22 +3,22 @@ CREATE DATABASE Sepomex
 USE Sepomex
 
 CREATE TABLE Estados (
-	id_estado INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	d_estado NVARCHAR(50),
-	c_estado NVARCHAR(5)
+	id_estado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	d_estado VARCHAR(50),
+	c_estado VARCHAR(5)
 )
 
 CREATE TABLE Municipios (
-	id_municipio INT IDENTITY(1,1)NOT NULL PRIMARY KEY ,
-	d_mnpio NVARCHAR(50)
+	id_municipio INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+	d_mnpio VARCHAR(50)
 )
 
 CREATE TABLE Colonias (
-	id_colonia INT IDENTITY(1,1) NOT NULL,
+	id_colonia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_estado INT NOT NULL,
 	id_municipio INT NOT NULL,
-	d_asenta NVARCHAR(50) NOT NULL,
-	d_codigo NVARCHAR(5)
+	d_asenta VARCHAR(50) NOT NULL,
+	d_codigo VARCHAR(5),
 	FOREIGN KEY (id_estado) REFERENCES Estados(id_estado),
 	FOREIGN KEY (id_municipio) REFERENCES Municipios(id_municipio)
 )
